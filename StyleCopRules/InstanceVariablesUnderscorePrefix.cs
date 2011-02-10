7f6c2128bb79a1524f57586f2e13687dc866f56f
Dis Shishkov <dis.shishkov.regs@outlook.com>
2011-02-10 16:49:59 +0300

    - 1st commit

---
+++ StyleCopRules/InstanceVariablesUnderscorePrefix.cs
@@ -0,0 +1,47 @@
+﻿namespace StyleCopRules
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.StyleCop;
+    using Microsoft.StyleCop.CSharp;
+
+    /// <summary>
+    /// This StyleCop Rule makes sure that instance variables are prefixed with an underscore.
+    /// </summary>
+    [SourceAnalyzer(typeof(CsParser))]
+    public sealed class InstanceVariablesUnderscorePrefix : SourceAnalyzer
+    {
+        /// <summary>
+        /// Analyzes the document.
+        /// </summary>
+        /// <param name="document">The document.</param>
+        public override void AnalyzeDocument(CodeDocument document)
+        {
+            CsDocument csdocument = (CsDocument)document;
+            if (csdocument.RootElement != null && !csdocument.RootElement.Generated)
+            {
+                csdocument.WalkDocument(new CodeWalkerElementVisitor<Object>(this.VisitElement), null, null);
+            }
+        }
+
+        /// <summary>
+        /// Visits the element.
+        /// </summary>
+        /// <param name="element">The element.</param>
+        /// <param name="parentElement">The parent element.</param>
+        /// <param name="context">The context.</param>
+        /// <returns>True if elemnt was visited.</returns>
+        private Boolean VisitElement(CsElement element, CsElement parentElement, Object context)
+        {
+            // Flag a violation if the instance variables are not prefixed with an underscore.
+            if (!element.Generated && element.ElementType == ElementType.Field && element.ActualAccess != AccessModifierType.Public 
+                && element.ActualAccess != AccessModifierType.Internal && element.Declaration.Name.ToCharArray()[0] != '_')
+            {
+                this.AddViolation(element, "InstanceVariablesUnderscorePrefix");
+            }
+            return true;
+        }
+    }
+}
