6d5bbf3feb7d56d9b98a4189edc8cd04be552e8f
Dis Shishkov <dis.shishkov.regs@outlook.com>
2011-02-23 12:40:49 +0300

    - add plug for 4 rules

---
+++ StyleCopRules/Rules.cs
@@ -0,0 +1,28 @@
+﻿namespace StyleCopRules
+{
+    /// <summary>
+    /// Represents the rules for StyleCop.
+    /// </summary>
+    internal enum Rules : sbyte
+    {
+        /// <summary>
+        /// This StyleCop rule makes sure that instance variables are prefixed with an underscore.
+        /// </summary>
+        InstanceVariablesUnderscorePrefix,
+
+        /// <summary>
+        /// This StyleCop rule makes sure that instance variables are class name or this.
+        /// </summary>
+        UseClassNameOrThis,
+
+        /// <summary>
+        /// This StyleCop rule not be allowed use linq aliases.
+        /// </summary>
+        DoNotUseLinqAliases,
+
+        /// <summary>
+        /// This StyleCop rule makes sure that instance variables are minimum length.
+        /// </summary>
+        MinimumFieldLength
+    }
+}
